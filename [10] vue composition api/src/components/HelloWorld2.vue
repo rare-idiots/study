5bf01705bc200975d21aa92c38bb263a98eb0915
WANJIN <wanjin911003@gmail.com>
2020-04-18 19:39:22 +0900

    10 vue composition api

---
+++ [10] vue composition api/src/components/HelloWorld2.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name:  'HelloWorld',
+  data() {
+    return {
+      message: 'hello'
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+</style>
